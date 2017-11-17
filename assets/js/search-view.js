19429cc1f42aebe66d929fb3d059f77144ab8ebe
InfiniteX777 <InfiniteX777@gmail.com>
2017-11-17 21:52:38 +0800

    the great division
    Created 'search-view.js'.

---
+++ assets/js/search-view.js
@@ -0,0 +1,69 @@
+var flight_type;
+
+function countInput(self) {
+	if (self.name === "adult") {
+		var children = document.getElementsByName("children")[0];
+		var infant = document.getElementsByName("infant")[0];
+		var v = parseInt(children.value);
+
+		if (parseInt(self.value) === 0 && v > 2) {
+			children.value = 2;
+		} else if (self.value >= 20 - v) {
+			self.value = 20 - v;
+		}
+
+		v = parseInt(infant.value);
+
+		if (v > self.value) {
+			infant.value = self.value;
+		}
+	} else {
+		var v = parseInt(
+			document.getElementsByName("adult")[0].value
+		);
+
+		if (self.name === "children") {
+			if (v === 0 && parseInt(self.value) > 2) {
+				self.value = 2;
+			} else if (self.value >= 20 - v) {
+				self.value = 20 - v;
+			}
+		} else {
+			if (self.value > v) {
+				self.value = v;
+			}
+		}
+	}
+}
+
+function flightTypeClick(self) {
+	if (flight_type) {
+		flight_type.parentElement.removeAttribute("style");
+	}
+
+	if (self.value == 0) {
+		var v = document.getElementsByName("date-return")[0];
+
+		v.setAttribute("disabled", "1");
+		v.setAttribute(
+			"style",
+			"cursor: not-allowed; color: var(--red); background-color: var(--red);"
+		);
+	} else {
+		var v = document.getElementsByName("date-return")[0];
+
+		v.removeAttribute("disabled");
+		v.removeAttribute("style");
+	}
+
+	self.setAttribute("checked", "checked");
+
+	self.parentElement.setAttribute(
+		"style",
+		"box-shadow: 0 0 10px #000; color: #fff; background-color: var(--red);"
+	);
+
+	flight_type = self;
+}
+
+flightTypeClick(document.getElementsByName("flight-type")[0]);
