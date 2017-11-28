7337dbeb28b13575e57b143383f3182b782cea1e
InfiniteX777 <InfiniteX777@gmail.com>
2017-11-28 22:28:33 +0800

    fix
    removed modal.
    removed cache.

--- assets/js/lemon.js
+++ assets/js/lemon.js
@@ -2,29 +2,23 @@ const {BrowserWindow} = require('electron');
 const path = require('path');
 const url = path.join('file://', __dirname, '../../assets/html/dialog.html');
 
-var _electronPrompt_col = {};
-
 this.prompt = function(parent, title, callback) {
-	let win = new BrowserWindow({
+	var win = new BrowserWindow({
 		width: 300,
-		height: 50,
-		useContentSize: true,
+		height: 100,
 		resizable: false,
 		minimizable: false,
 		maximizable: false,
 		fullscreenable: false,
 		title: title,
-		parent: parent,
-		modal: true
+		parent: parent
 	});
 
 	win.setMenu(null);
 	win.loadURL(url);
 	win.callback = callback;
-	_electronPrompt_col[win] = 1;
 
 	win.on('closed', () => {
-		_electronPrompt_col[win] = null;
 		win = null;
 	})
 
