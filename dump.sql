c7b1e27ed56ec5921d05e219e9ff23aafc109b26
InfiniteX777 <InfiniteX777@gmail.com>
2017-12-06 01:34:19 +0800

    simplified
    removed account.return, account_ofw_num.
    arranged account attributes to match the software.
    changed most int to varchar.
    added random lulz.

--- dump.sql
+++ dump.sql
@@ -24,40 +24,36 @@ DROP TABLE IF EXISTS `account`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `gender` int(1) NOT NULL,
   `name_first` varchar(45) NOT NULL,
-  `name_midd` varchar(45) DEFAULT NULL,
   `name_last` varchar(45) NOT NULL,
-  `birthdate` date NOT NULL,
+  `name_midd` varchar(45) DEFAULT NULL,
+  `gender` int(1) NOT NULL,
   `nationality` varchar(45) NOT NULL,
-  `ofw_num` varchar(45) DEFAULT NULL,
-  `passport_num` varchar(45) DEFAULT NULL,
-  `passport_country_of_issue` varchar(45) DEFAULT NULL,
-  `passport_exp_date` date DEFAULT NULL,
+  `birthdate` date NOT NULL,
   `email` varchar(45) NOT NULL,
-  `contact0_country_code` int(11) NOT NULL,
-  `contact0_area_code` int(11) NOT NULL,
-  `contact0_num` int(11) NOT NULL,
-  `contact1_country_code` int(11) NOT NULL,
-  `contact1_area_code` int(11) NOT NULL,
-  `contact1_num` int(11) NOT NULL,
-  `payment` int(1) NOT NULL DEFAULT '0',
-  `card_num` varchar(45) DEFAULT NULL,
-  `card_val_num` varchar(45) DEFAULT NULL,
-  `card_exp` date DEFAULT NULL,
-  `card_name_first` varchar(45) DEFAULT NULL,
-  `card_name_last` varchar(45) DEFAULT NULL,
-  `card_address` varchar(90) DEFAULT NULL,
-  `card_zip` varchar(45) DEFAULT NULL,
-  `card_country` varchar(45) DEFAULT NULL,
-  `card_region` varchar(45) DEFAULT NULL,
-  `card_email` varchar(45) DEFAULT NULL,
-  `card_country_code` varchar(45) DEFAULT NULL,
-  `card_area_code` varchar(45) DEFAULT NULL,
-  `card_contact_num` varchar(45) DEFAULT NULL,
+  `contact0_area_code` varchar(45) NOT NULL,
+  `contact0_num` varchar(45) NOT NULL,
+  `contact1_area_code` varchar(45) DEFAULT NULL,
+  `contact1_num` varchar(45) DEFAULT NULL,
+  `passport_num` varchar(45) NOT NULL,
+  `passport_country_of_issue` varchar(45) NOT NULL,
+  `passport_exp_date` date NOT NULL,
+  `card_num` varchar(45) NOT NULL,
+  `card_val_num` varchar(45) NOT NULL,
+  `card_exp` date NOT NULL,
+  `card_name_first` varchar(45) NOT NULL,
+  `card_name_last` varchar(45) NOT NULL,
+  `card_address` varchar(90) NOT NULL,
+  `card_zip` varchar(45) NOT NULL,
+  `card_city` varchar(45) NOT NULL,
+  `card_country` varchar(45) NOT NULL,
+  `card_region` varchar(45) NOT NULL,
+  `card_email` varchar(45) NOT NULL,
+  `card_area_code` varchar(45) NOT NULL,
+  `card_contact_num` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='payment[0 = credit card, 1 = paypal]\n';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='payment[0 = credit card, 1 = paypal]\n';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,6 +62,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (1,'a','b','c',1,'w','2017-06-01','em','123','123','123','123','123','123','2017-06-01','123','123','2017-06-01','1','2','123','123','123','123','123','123','123','123'),(2,'a','b','c',0,'c','2107-12-02','a','123','456','123','456','123','456','2017-02-03','123','456','2017-04-01','a','b','c','d','e','f','g','h','234','567');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,19 +152,18 @@ DROP TABLE IF EXISTS `flight`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `flight` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `aircraft_id` int(11) DEFAULT NULL,
   `from` varchar(45) NOT NULL,
   `to` varchar(45) NOT NULL,
   `depart` datetime NOT NULL,
-  `return` datetime DEFAULT NULL,
   `passenger_max` int(11) NOT NULL,
   `fare_seat` float NOT NULL,
   `fare_baggage` float DEFAULT NULL,
   `fare_meal` float DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='fare:\n	null = not available.\n	meal needs baggage to be available.\n	seat is always available, otherwise flight should not be created.\n\n*total passenger is taken from ''book'' table.\n*max passenger does not necessarily need to be the same as ''aircraft'' capacity.';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='fare:\n	null = not available.\n	meal needs baggage to be available.\n	seat is always available, otherwise flight should not be created.\n\n*total passenger is taken from ''book'' table.\n*max passenger does not necessarily need to be the same as ''aircraft'' capacity.';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -176,6 +172,7 @@ CREATE TABLE `flight` (
 
 LOCK TABLES `flight` WRITE;
 /*!40000 ALTER TABLE `flight` DISABLE KEYS */;
+INSERT INTO `flight` VALUES (0000000001,24,'Manila','Singapore','2017-05-06 15:59:59',59,900,1000,1500);
 /*!40000 ALTER TABLE `flight` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -188,4 +185,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-28 22:21:50
+-- Dump completed on 2017-12-06  1:31:25
