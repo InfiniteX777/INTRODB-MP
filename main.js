701fc8edb4b1e01ce6f057ad571ed6be1e387c89
InfiniteX777 <InfiniteX777@gmail.com>
2017-12-09 20:26:19 +0800

    frameless window

--- main.js
+++ main.js
@@ -8,20 +8,19 @@ function createWindow () {
 	// Create the browser window.
 	win = new BrowserWindow({
 		width: 800,
-		height: 800
+		height: 800,
+		resizable: false,
+		maximizable: false,
+		fullscreenable: false,
+		title: "INTRODB-MP",
+		frame: false
 	})
 
-	// and load the index.html of the app.
+	win.setMenu(null);
+
 	win.loadURL(path.join(__dirname, 'index.html'));
 
-	// Open the DevTools.
-	//win.webContents.openDevTools()
-
-	// Emitted when the window is closed.
 	win.on('closed', () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
 		win = null
 	})
 }
