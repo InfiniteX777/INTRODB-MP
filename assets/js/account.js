c501352aa0e27e40b14f5300054b97ef8e4e6c8d
InfiniteX777 <InfiniteX777@gmail.com>
2017-12-09 20:29:05 +0800

    implemented account.html

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,36 +1,175 @@
-let elm = [];
-let prefix = [
-	"Hello there, ",
-	"Last logged ",
-	"Email: ",
-	"Nationality: "
+var account_dat = parent.account_dat;
+var input = document.getElementsByTagName("input");
+var elm_h2 = document.getElementsByTagName("h2")[0];
+var elm_h3 = document.getElementsByTagName("h3")[0];
+var elm_h4 = document.getElementsByTagName("h4")[0];
+var iframe = parent.document
+	.getElementsByTagName("iframe")[1];
+var welcome_index = [
+	"Hey there, <name>!",
+	"What's up, <name>?",
+	"How's it going, <name>?",
+	"Anything today, <name>?",
+	"What can I do for you, <name>?",
+	"Looking good, <name>!",
+	"Good day, <name>!",
+	"Welcome, <name>.",
+	"Nice seeing you, <name>!",
+	"Another beautiful day, <name>!",
+	"Good to see you, <name>."
+];
+var dat_index = [
+	"name_first",
+	"name_midd",
+	"name_last",
+	"email"
 ];
 
-elm[0] = document.createElement("h2");
-document.body.appendChild(elm[0]);
+console.log(account_dat.last_log);
 
-for (i = 1; i <= 4; i++) {
-	elm[i] = document.createElement("h3");
-	document.body.appendChild(elm[i]);
+// Setup account log (this applies to booking as well).
+elm_h3.innerHTML =
+	"Last logged: " +
+	account_dat.last_log.toLocaleString();
+
+// Update the log date.
+var con = parent.connection;
+var t = new Date();
+
+if (con) {
+	con.query(
+		"UPDATE credential\
+			SET\
+				last_log = \"" +
+					t.getFullYear() + "-" +
+					(t.getMonth()+1) + "-" +
+					t.getDate() + " " +
+					t.getHours() + ":" +
+					t.getMinutes() + ":" +
+					t.getSeconds() +
+				"\"\
+			WHERE\
+				account_id = " + account_dat.id,
+		(err) => {
+			if (err) {
+				console.log(err);
+			}
+		}
+	)
 }
 
-function updateDetails(detail) {
-	elm[0].innerHTML = 
-		prefix[0] +
-		(detail[1] == 0 ? "Mr. " : "Ms. ") +
-		detail[0] + "!";
+// Politeness.
+function greet() {
+	elm_h2.innerHTML =
+		welcome_index[
+			Math.floor(Math.random()*welcome_index.length)
+		].replace("<name>", account_dat.name_first);
+}
 
-	for (i = 1; i <= 3; i++) {
-		elm[i].innerHTML =
-		prefix[i] +
-		detail[i + 1];
+greet();
+
+// Setup the user details.
+function set_input() {
+	for (i = 0; i < 4; i++) {
+		input[i].value = account_dat[dat_index[i]];
 	}
 }
 
-updateDetails([
-	"Bob Ross",
-	0,
-	"9:00 PM 11/25/2017",
-	"bob_ross@bob.com",
-	"unknown"
-]);
+// Reset
+input[6].addEventListener("click", set_input);
+
+// Update
+input[7].addEventListener("click", (event) => {
+	var con = parent.connection;
+
+	if (con) {
+		elm_h4.innerHTML = "";
+
+		con.query(
+			"UPDATE account a\
+				SET\
+					name_first = \"" +
+						input[0].value + "\",\
+					name_midd = \"" +
+						input[1].value + "\",\
+					name_last = \"" +
+						input[2].value + "\",\
+					email = \"" +
+						input[3].value + "\"\
+				WHERE\
+					a.id = " + account_dat.id,
+			(err) => {
+				if (!err) {
+					for (i = 0; i < 4; i++) {
+						account_dat[dat_index[i]] =
+							input[i].value;
+					}
+
+					greet();
+
+					elm_h4.innerHTML += " Account \
+						details updated successfully!";
+				} else {
+					elm_h4.innerHTML += " Something went \
+						wrong with your account \
+						details update...";
+					console.log(err);
+				}
+			}
+		);
+
+		// Password!
+		if (input[4].value.length >= 6) {
+			if (input[5].value === account_dat.password) {
+				con.query(
+					"UPDATE credential a\
+						SET\
+							password = \"" +
+								input[4].value + "\"\
+						WHERE\
+							a.account_id = " +
+								account_dat.id,
+					(err) => {
+						if (!err) {
+							account_dat.password =
+								input[4].value;
+							elm_h4.innerHTML +=
+								" Password successfully \
+								updated!";
+						} else {
+							elm_h4.innerHTML +=
+								" Something went wrong \
+								with your password \
+								update...";
+							console.log(err);
+						}
+					}
+				)
+			} else {
+				elm_h4.innerHTML += " Incorrect password!";
+			}
+		}
+	} else {
+		elm_h4.innerHTML = "No connection...";
+	}
+})
+
+// Logout
+input[8].addEventListener("click", (event) => {
+	delete parent.account_dat;
+
+	iframe.setAttribute(
+		"src",
+		"./assets/html/login.html"
+	);
+})
+
+// Flight History
+input[9].addEventListener("click", (event) => {
+	iframe.setAttribute(
+		"src",
+		"./assets/html/history.html"
+	);
+})
+
+set_input();
