e976da4e0c88b7c336c6610123bdc3a56258ef13
InfiniteX777 <InfiniteX777@gmail.com>
2017-12-10 16:57:21 +0800

    cleaned data.
    restructured tables.
    added 'record' table.
    added 'create' attribute to most tables.

--- dump.sql
+++ dump.sql
@@ -51,9 +51,10 @@ CREATE TABLE `account` (
   `card_email` varchar(45) NOT NULL,
   `card_area_code` varchar(45) NOT NULL,
   `card_contact_num` varchar(45) NOT NULL,
+  `create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='payment[0 = credit card, 1 = paypal]\n';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='payment[0 = credit card, 1 = paypal]\n';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,33 +63,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (1,'a','b','c',1,'w','2017-06-01','em','123','123','123','123','123','123','2017-06-01','123','123','2017-06-01','1','2','123','123','123','123','123','123','123','123'),(2,'a','b','c',0,'c','2107-12-02','a','123','456','123','456','123','456','2017-02-03','123','456','2017-04-01','a','b','c','d','e','f','g','h','234','567');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `aircraft`
---
-
-DROP TABLE IF EXISTS `aircraft`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `aircraft` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `pilot_id` int(11) DEFAULT NULL,
-  `capacity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `aircraft`
---
-
-LOCK TABLES `aircraft` WRITE;
-/*!40000 ALTER TABLE `aircraft` DISABLE KEYS */;
-/*!40000 ALTER TABLE `aircraft` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -100,16 +75,16 @@ DROP TABLE IF EXISTS `book`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `book` (
   `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
-  `flight_id_depart` int(11) NOT NULL,
-  `flight_id_return` int(11) DEFAULT NULL,
-  `account_id` int(11) DEFAULT NULL,
-  `account_parent_id` int(11) DEFAULT NULL,
-  `seat_loc` varchar(4) NOT NULL,
+  `flight_id` int(10) NOT NULL,
+  `account_id` int(10) DEFAULT NULL,
+  `seat` varchar(4) NOT NULL,
   `bag_meal_auth` int(1) NOT NULL DEFAULT '0',
   `cost` float NOT NULL,
+  `cost_tax` float NOT NULL,
+  `cost_flight_charge` float NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='!flight_id_return ? one-way : round-trip/multi-city\n\nbag_meal_auth[0 = fly, 1 = fly+bag, 2 = fly+bag+meal]';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='!flight_id_return ? one-way : round-trip/multi-city\n\nbag_meal_auth[0 = fly, 1 = fly+bag, 2 = fly+bag+meal]';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,7 +93,6 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (0000000001,1,1,1,1,'1',1,1);
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,7 +105,9 @@ DROP TABLE IF EXISTS `credential`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `credential` (
   `account_id` int(10) NOT NULL,
-  `password` varchar(16) NOT NULL
+  `password` varchar(16) NOT NULL,
+  `last_log` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -153,17 +129,18 @@ DROP TABLE IF EXISTS `flight`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `flight` (
   `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
-  `aircraft_id` int(11) DEFAULT NULL,
   `from` varchar(45) NOT NULL,
   `to` varchar(45) NOT NULL,
   `depart` datetime NOT NULL,
-  `passenger_max` int(11) NOT NULL,
   `fare_seat` float NOT NULL,
   `fare_baggage` float DEFAULT NULL,
   `fare_meal` float DEFAULT NULL,
+  `tax` float NOT NULL,
+  `flight_charge` float NOT NULL,
+  `create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='fare:\n	null = not available.\n	meal needs baggage to be available.\n	seat is always available, otherwise flight should not be created.\n\n*total passenger is taken from ''book'' table.\n*max passenger does not necessarily need to be the same as ''aircraft'' capacity.';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='fare:\n	null = not available.\n	meal needs baggage to be available.\n	seat is always available, otherwise flight should not be created.\n\n*total passenger is taken from ''book'' table.\n*max passenger does not necessarily need to be the same as ''aircraft'' capacity.';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -172,8 +149,40 @@ CREATE TABLE `flight` (
 
 LOCK TABLES `flight` WRITE;
 /*!40000 ALTER TABLE `flight` DISABLE KEYS */;
-INSERT INTO `flight` VALUES (0000000001,24,'Manila','Singapore','2017-05-06 15:59:59',59,900,1000,1500);
 /*!40000 ALTER TABLE `flight` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `record`
+--
+
+DROP TABLE IF EXISTS `record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `record` (
+  `create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `scope_from` date NOT NULL,
+  `scope_to` date NOT NULL,
+  `acc_new` int(11) NOT NULL,
+  `acc_reg` int(11) NOT NULL,
+  `acc_act` int(11) NOT NULL,
+  `acc_all` int(11) NOT NULL,
+  `fli_new` int(11) NOT NULL,
+  `fli_act` int(11) NOT NULL,
+  `fli_all` int(11) NOT NULL,
+  `visited` varchar(45) NOT NULL,
+  `immigrant` varchar(45) NOT NULL,
+  `income` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `record`
+--
+
+LOCK TABLES `record` WRITE;
+/*!40000 ALTER TABLE `record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `record` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -185,4 +194,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-06  1:31:25
+-- Dump completed on 2017-12-10 16:54:56
